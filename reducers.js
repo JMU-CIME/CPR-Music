e7138d41f6c7cc23f0fcd8cc97d9856f9e9b50a2
Zamua Nasrawt <zamua@nasrawt.io>
2022-01-06 14:18:15 -0500

    add mock data

--- reducers.js
+++ reducers.js
@@ -34,83 +34,36 @@ const timerReducer = (state = initialTimerState, { type, payload }) => {
   }
 };
 
-const mockCourses = [
+const mockAssignments = [
   {
     id: 0,
-    name: "7th Grade Band",
+    activityId: 0,
+    userId: 0,
+    deadline: "exists",
   },
   {
     id: 1,
-    name: "8th Grade Orchestra",
+    activityId: 1,
+    userId: 1,
   },
-];
+]
 
-const coursesReducer = (state = mockCourses, { type, payload }) => {
-  switch (type) {
-    case types.ADDED_COURSE:
-      console.log('payload', payload)
-      return [...state, { id: state.length, name: payload.name }];
-  }
-  return state;
-};
-
-const loginStatus = {
-  isStudent: false,
-  isTeacher: false,
-  isLoggedOut: true,
-};
-
-const loginReducer = (state = loginStatus, { type, payload }) => {
-  switch (type) {
-    case types.REMEMBER_LOGIN:
-      return payload;
-  }
-  return state;
-};
-
-const initialPieces = [
+const mockActivities = [
+  {
+    id: 0,
+    typeId: 1,
+    pieceId: 0,
+    body: "body0"
+  },
   {
     id: 1,
-    slug: "celebration-for-a-new-day",
-    name: "Celebration for a New Day",
-  },
-  {
-    id: 2,
-    slug: "air-for-band",
-    name: "Air for Band",
-  },
-  {
-    id: 3,
-    slug: "america-the-beautiful",
-    name: "America the Beautiful",
-  },
-  {
-    id: 4,
-    slug: "chant",
-    name: "Chant",
-  },
-  {
-    id: 5,
-    slug: "danny-boy",
-    name: "Danny Boy",
-  },
-  {
-    id: 6,
-    slug: "jubilo",
-    name: "Jubilo",
-  },
-  {
-    id: 7,
-    slug: "portamento",
-    name: "Portamento",
-  },
-];
+    typeId: 2,
+    pieceId: 1,
+    body: "body1"
+  }
+]
 
-const piecesReducer = (state = initialPieces, { type, payload }) => {
-  return state;
-};
-
-const initialActivityTypes = [
+const mockActivityTypes = [
   {
     id: 1,
     name: "Melody",
@@ -134,11 +87,218 @@ const initialActivityTypes = [
 ];
 
 const activityTypesReducer = (
-  state = initialActivityTypes,
+  state = mockActivityTypes,
   { type, payload }
 ) => {
   return state;
 };
+
+const Level = {
+  Primary: 'Primary',
+  Secondary: 'Secondary'
+}
+
+const mockPieces = [
+  {
+    id: 0,
+    slug: "piece-with-id-0",
+    name: "Piece with id 0",
+    composerId: 0,
+    videoS3Key: 'video/0',
+    audioS3Key: 'audio/0',
+    notationS3Key: 'notation/0',
+    date: 'timestamp0',
+    level: Level.Primary,
+  },
+  {
+    id: 1,
+    slug: "celebration-for-a-new-day",
+    name: "Celebration for a New Day",
+    composerId: 0,
+    videoS3Key: 'video/1',
+    audioS3Key: 'audio/1',
+    notationS3Key: 'notation/1',
+    date: 'timestamp1',
+    level: Level.Primary,
+  },
+  {
+    id: 2,
+    slug: "air-for-band",
+    name: "Air for Band",
+    composerId: 0,
+    videoS3Key: 'video/2',
+    audioS3Key: 'audio/2',
+    notationS3Key: 'notation/2',
+    date: 'timestamp2',
+    level: Level.Primary,
+  },
+  {
+    id: 3,
+    slug: "america-the-beautiful",
+    name: "America the Beautiful",
+    composerId: 0,
+    videoS3Key: 'video/3',
+    audioS3Key: 'audio/3',
+    notationS3Key: 'notation/3',
+    date: 'timestamp3',
+    level: Level.Primary,
+  },
+  {
+    id: 4,
+    slug: "chant",
+    name: "Chant",
+    composerId: 1,
+    videoS3Key: 'video/4',
+    audioS3Key: 'audio/4',
+    notationS3Key: 'notation/4',
+    date: 'timestamp4',
+    level: Level.Primary,
+  },
+  {
+    id: 5,
+    slug: "danny-boy",
+    name: "Danny Boy",
+    composerId: 1,
+    videoS3Key: 'video/5',
+    audioS3Key: 'audio/5',
+    notationS3Key: 'notation/5',
+    date: 'timestamp5',
+    level: Level.Primary,
+  },
+  {
+    id: 6,
+    slug: "jubilo",
+    name: "Jubilo",
+    composerId: 1,
+    videoS3Key: 'video/6',
+    audioS3Key: 'audio/6',
+    notationS3Key: 'notation/6',
+    date: 'timestamp6',
+    level: Level.Secondary,
+  },
+  {
+    id: 7,
+    slug: "portamento",
+    name: "Portamento",
+    composerId: 1,
+    videoS3Key: 'video/7',
+    audioS3Key: 'audio/7',
+    notationS3Key: 'notation/7',
+    date: 'timestamp7',
+    level: Level.Secondary,
+  },
+];
+
+const piecesReducer = (state = mockPieces, { type, payload }) => {
+  return state;
+};
+
+const mockComposers = [
+  {
+    id: 0,
+    name: "name0",
+  },
+  {
+    id: 1,
+    name: "name1"
+  }
+]
+
+const mockUsers = [
+  {
+    id: 0,
+    name: "name0",
+    grade: "grade0",
+  },
+  {
+    id: 1,
+    name: "name1",
+    grade: "grade1",
+  },
+]
+
+const mockCourses = [
+  {
+    id: 0,
+    name: "7th Grade Band",
+    ownerId: 0,
+    level: Level.Secondary,
+  },
+  {
+    id: 1,
+    name: "8th Grade Orchestra",
+    ownerId: 1,
+    level: Level.Primary,
+  },
+];
+
+const coursesReducer = (state = mockCourses, { type, payload }) => {
+  switch (type) {
+    case types.ADDED_COURSE:
+      console.log('payload', payload)
+      return [...state, { id: state.length, name: payload.name }];
+  }
+  return state;
+};
+
+const Role = {
+  student: "Student",
+  teacher: "Teacher"
+}
+const mockEnrollments = [
+  {
+    id: 0,
+    userId: 0,
+    courseId: 0,
+    role: Role.student
+  },
+  {
+    id: 1,
+    userId: 1,
+    courseId: 1,
+    role: Role.teacher
+  }
+]
+
+const mockSubmissions = [
+  {
+    userId: 0,
+    asignmentId: 0,
+    content: "content0"
+  },
+  {
+    userId: 1,
+    asignmentId: 1,
+    content: "content1"
+  }
+]
+
+const mockSubmissionAttachments = [
+  {
+    userId: 0,
+    contentS3Key: "content/0",
+  },
+  {
+    userId: 1,
+    contentS3Key: "content/1",
+  },
+]
+
+const loginStatus = {
+  isStudent: false,
+  isTeacher: false,
+  isLoggedOut: true,
+};
+
+const loginReducer = (state = loginStatus, { type, payload }) => {
+  switch (type) {
+    case types.REMEMBER_LOGIN:
+      return payload;
+  }
+  return state;
+};
+
+
 
 // stubbing out pages
 // ✅ Login user
