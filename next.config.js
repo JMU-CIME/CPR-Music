d00dbbc571c4afdddac77ccadea1a11cdcb4a7cd
Michael Stewart <stewarmc@jmu.edu>
2022-01-14 16:05:53 -0500

    how does this url thng work

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: "/auth-token/:rest*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_HOST}/:rest*`,
+      },
+      {
         source: "/backend/:rest*",
-        destination: "http://localhost:8000/:rest*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_HOST}/:rest*`,
       },
     ];
   },
