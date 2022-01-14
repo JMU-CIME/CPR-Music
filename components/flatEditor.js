7637f2056d5fe55ed56ca3c6154035eb92fa0dae
Michael Stewart <stewarmc@jmu.edu>
2022-01-14 13:01:44 -0500

    change piece easier

--- components/flatEditor.js
+++ components/flatEditor.js
@@ -11,14 +11,31 @@ const FlatEditor = (props) => {
   const refreshJSON = () => {
     embed.getJSON().then((jsonData) => setJson(JSON.stringify(jsonData)));
   };
+
+  const scores = {
+    "Air for Band - Melody - Bb": {
+      score: "61e09029dffcd50014571a80",
+      sharingKey:
+        "169f2baebaa5721b4442b124fc984cce26f7e63312fb597d187f9c4d0e3aa1897df093c3bec76af250eb3ca0f36eb4f645ac70f470a695ccd217a1ce0cd52120",
+    },
+  };
+
+  const currentPiece = "Air for Band";
+  const currentPart = "Melody";
+  const currentTransposition = "Bb";
+
+  const currentPath = [currentPiece, currentPart, currentTransposition].join(
+    " - "
+  );
+
   useEffect(() => {
     embed = new Embed(editorRef.current, {
-      score: "617052dca7f3100012392bb6",
+      score: scores[currentPath].score,
       height: "450",
+      width: "100%",
       embedParams: {
         mode: "edit",
-        sharingKey:
-          "6e24af728f6029fcc69f05aa95f3e0fd6b87c613b54b39b1ae0f123cc698c5d69633c7625ef194f2bd0f6f73cc923a4716bad2283642bd0cad0f4335e6777410",
+        sharingKey: scores[currentPath].sharingKey,
         appId: "60a51c906bcde01fc75a3ad0",
         controlsPosition: "bottom",
       },
