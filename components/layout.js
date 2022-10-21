fe7824e33f47fa016eae4e6c171080a6eeaf1d68
Michael Stewart <stewarmc@jmu.edu>
2022-10-20 21:53:38 -0400

    update logo

--- components/layout.js
+++ components/layout.js
@@ -56,14 +56,14 @@ export default function Layout({ children }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="MusicCPR - Create, Perform, Respond, and Connect" />
         <meta property="og:description" content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes." />
-        <meta property="og:image" content="/MusicCPR logo-rect-social-resized.png" />
+        <meta property="og:image" content="/MusicCPR-logo.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="musiccpr.org" />
         <meta property="twitter:url" content="/" />
         <meta name="twitter:title" content="MusicCPR - Create, Perform, Respond, and Connect" />
         <meta name="twitter:description" content="MusicCPR facilitates music teachers' collection of individual student achievement data that aligns with ensemble repertoire and artistic processes." />
-        <meta name="twitter:image" content="/MusicCPR logo-rect-social-resized.png" />
+        <meta name="twitter:image" content="/MusicCPR-logo.png" />
       </Head>
       <Navigation />
       {(!PUBLIC_PATHS.includes(router.pathname) && userLoaded && token) ||
