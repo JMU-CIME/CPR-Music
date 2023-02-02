54ab708c0f04c9428494b1a340dce070c6a60054
Alex Dumouchelle <58530312+AlexDumo@users.noreply.github.com>
2023-02-02 13:25:00 -0500

    Made the links for  "about" open in new tab.

--- pages/about.js
+++ pages/about.js
@@ -30,16 +30,16 @@ function About() {
         MusicCPR is a free web-based platform to promote standards-based
         instrumental music education. MusicCPR aligns with four artistic
         processes found in{' '}
-        <a href="https://nafme.org/my-classroom/standards/">
+        <a target="_blank" rel="noopener noreferrer" href="https://nafme.org/my-classroom/standards/">
           National Standards for Music Education
         </a>
         : create, perform, respond, and connect and their manifestations in
         state standards for instrumental music education (e.g., New York's{' '}
-        <a href="http://www.nysed.gov/curriculum-instruction/arts-standards-implementation-resources">
+        <a target="_blank" rel="noopener noreferrer" href="http://www.nysed.gov/curriculum-instruction/arts-standards-implementation-resources">
           Arts Learning Standards
         </a>
         , Virginia's{' '}
-        <a href="https://doe.virginia.gov/testing/sol/standards_docs/fine_arts/2020/2020fasol-music.pdf">
+        <a target="_blank" rel="noopener noreferrer" href="https://doe.virginia.gov/testing/sol/standards_docs/fine_arts/2020/2020fasol-music.pdf">
           Music Standards of Learning
         </a>
         ).
@@ -56,18 +56,18 @@ function About() {
       <p>
         MusicCPR is housed at James Madison University, in a collaboration
         between the{' '}
-        <a href="https://jmu.edu/cs">Department of Computer Science</a> and the{' '}
-        <a href="https://www.jmu.edu/arts/ocp/index.shtml">
+        <a target="_blank" rel="noopener noreferrer" href="https://jmu.edu/cs">Department of Computer Science</a> and the{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://www.jmu.edu/arts/ocp/index.shtml">
           Office of Creative Propulsion
         </a>{' '}
         with collaborators at{' '}
-        <a href="https://rochester.edu/">University of Rochester</a>'s{' '}
-        <a href="https://www.esm.rochester.edu/">Eastman School of Music</a> and
-        in <a href="https://www.udel.edu/">University of Delaware</a>'s{' '}
-        <a href="https://www.music.udel.edu/">School of Music</a>. MusicCPR's
+        <a target="_blank" rel="noopener noreferrer" href="https://rochester.edu/">University of Rochester</a>'s{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://www.esm.rochester.edu/">Eastman School of Music</a> and
+        in <a target="_blank" rel="noopener noreferrer" href="https://www.udel.edu/">University of Delaware</a>'s{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://www.music.udel.edu/">School of Music</a>. MusicCPR's
         development has been supported by these institutions, as well as{' '}
-        <a href="https://4-va.org/">4-VA Collaborative</a> and{' '}
-        <a href="https://nafme.org/">
+        <a target="_blank" rel="noopener noreferrer" href="https://4-va.org/">4-VA Collaborative</a> and{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://nafme.org/">
           National Association for Music Education
         </a>
         .
@@ -75,41 +75,32 @@ function About() {
       <p>
         If you're interested in trying MusicCPR, or have a question for the
         team, don't hesitate to drop us a line at{' '}
-        <a href="mailto:feedback@musiccpr.org">feedback@musiccpr.org</a>
+        <a target="_blank" rel="noopener noreferrer" href="mailto:feedback@musiccpr.org">feedback@musiccpr.org</a>
       </p>
-      {/* </Col>
-        <Col sm={3}>
-          <h3>Support MusicCPR</h3>
-          <p>
-            If you would be interested in supporting our project, click below.
-          </p>
-          <a href="https://dukesfunder.jmu.edu/project/30270" target="_blank" rel="noopener noreferrer" className='btn btn-info'> Learn more</a>
-        </Col>
-      </Row> */}
-
+      
       <h2>Investigators</h2>
 
       <ul>
         <li>
-          <a href="https://www.esm.rochester.edu/directory/caravan-lisa/">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.esm.rochester.edu/directory/caravan-lisa/">
             Lisa R. Caravan, DMA
           </a>{' '}
           (Assistant Professor, Department of Music Teaching and Learning,
           Eastman School of Music, University of Rochester)
         </li>
         <li>
-          <a href="https://www.esm.rochester.edu/directory/snell-alden/">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.esm.rochester.edu/directory/snell-alden/">
             Alden H. Snell, II, Ph.D.
           </a>{' '}
           (Associate Professor, Department of Music Teaching and Learning,
           Eastman School of Music, University of Rochester)
         </li>
         <li>
-          <a href="https://hcientist.com">Michael C. Stewart, Ph.D.</a>{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://hcientist.com">Michael C. Stewart, Ph.D.</a>{' '}
           (Assistant Professor of Computer Science, James Madison University)
         </li>
         <li>
-          <a href="https://www.jmu.edu/arts/people/stringham-david.shtml">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.jmu.edu/arts/people/stringham-david.shtml">
             David A. Stringham, Ph.D.
           </a>{' '}
           (Professor of Music; Executive Director, Office of Creative
@@ -123,12 +114,12 @@ function About() {
           University)
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/alexdumo/">Alex Dumouchelle</a>{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://portfolium.com/AlexDumo" >Alex Dumouchelle</a>{' '}
           (Undergraduate Student, James Madison University)
         </li>
         <li>Jonah Giblin (Undergraduate Student, James Madison University)</li>
         <li>
-          <a href="http://benguerrero.com/">Benjamin Guerrero, MM</a> (Preparing
+          <a target="_blank" rel="noopener noreferrer" href="http://benguerrero.com/">Benjamin Guerrero, MM</a> (Preparing
           Future Faculty Fellow, James Madison University; Ph.D. Candidate,
           University of Rochester)
         </li>
@@ -151,7 +142,7 @@ function About() {
         </li>
         <li>Zamua Nasrawt (Consulting Musician and Web Developer)</li>
         <li>
-          <a href="https://github.com/LiemKN/">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/LiemKN/">
             Liem Nguyen (Undergraduate Student, James Madison University)
           </a>
         </li>
@@ -159,7 +150,7 @@ function About() {
           Meara Patterson (Undergraduate Student, James Madison University)
         </li>
         <li>
-          <a href="https://www.jmu.edu/cise/people/faculty/riley-philip.shtml">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.jmu.edu/cise/people/faculty/riley-philip.shtml">
             Phil Riley
           </a>{' '}
           (Lecturer in Computer Science, James Madison University)
@@ -167,13 +158,13 @@ function About() {
         <li>Isaiah Ortiz (Undergraduate Student, James Madison University)</li>
         <li>Nathan Self (Consulting Musician and Web Developer)</li>
         <li>
-          <a href="http://pawelwozniak.eu/">Paweł W. Woźniak, Ph.D.</a>{' '}
+          <a target="_blank" rel="noopener noreferrer" href="http://pawelwozniak.eu/">Paweł W. Woźniak, Ph.D.</a>{' '}
           (Associate Professor, Interaction Design and Software Engineering
           division, Department of Computer Science and Engineering, Chalmers
           University)
         </li>
         <li>
-          <a href="https://laurenyz.github.io/portfolio/">Lauren Yu</a> (Web
+          <a target="_blank" rel="noopener noreferrer" href="https://laurenyz.github.io/portfolio/">Lauren Yu</a> (Web
           Developer)
         </li>
       </ul>
