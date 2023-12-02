89f7e518f98c09bb5fd069e8333d3c03ad553eac
Michael Stewart <tgm@vt.edu>
2023-12-01 22:38:10 -0500

    dynamically generate buckets for aural

--- components/flatEditor.js
+++ components/flatEditor.js
@@ -35,6 +35,9 @@ function FlatEditor({
   giveJSON,
   trim,
   colors,
+  referenceScoreJSON,
+  chordScaleBucket,
+  instrumentName,
 }) {
   // console.log('flat io embed log', scoreJSON, orig);
   const [json, setJson] = useState('');
@@ -43,6 +46,467 @@ function FlatEditor({
   const editorRef = React.createRef();
   const [addingNote, setAddingNote] = useState(false);
 
+  // *FIX* Need to get rid of arrow, make it a normal function as we did with keyFromScoreJSON, and getChordScaleInKey below.
+  const embedTransposed = (
+    bucket,
+    embed,
+    // templt,
+    keySig,
+    instrName,
+    octaveShift
+  ) => {
+    const template = JSON.parse(
+      JSON.stringify({
+        'score-partwise': {
+          'part-list': {
+            'score-part': [
+              {
+                'part-name': '',
+                voiceMapping: { 0: [0] },
+                staffMapping: [
+                  {
+                    mainVoiceIdx: 0,
+                    voices: [0],
+                    staffUuid: 'staffUuid',
+                  },
+                ],
+                voiceIdxToUuidMapping: {
+                  0: 'voiceUuid',
+                },
+                voiceUuidToIdxMapping: {
+                  voiceUuid: 0,
+                },
+                'part-abbreviation': '',
+                'score-instrument': {
+                  'instrument-name': '',
+                  $id: 'P1-I1',
+                },
+
+                $id: 'P1',
+                uuid: 'P1',
+              },
+            ],
+          },
+          part: [
+            {
+              measure: [
+                {
+                  note: [],
+                  $number: '1',
+                  barline: {
+                    'bar-style': 'light-heavy',
+                    noteBefore: 4,
+                  },
+                  attributes: [
+                    {
+                      time: { beats: '5', 'beat-type': '4' },
+                      clef: {},
+                      key: {},
+                      'staff-details': { 'staff-lines': '5' },
+                    },
+                  ],
+                },
+              ],
+              $id: 'P1',
+              uuid: 'P1',
+            },
+          ],
+        },
+      })
+    );
+    // const template = templt
+    // change the notes in the score from whatever they are in tonic and eb to what we're given
+    const scorePart =
+      template?.['score-partwise']?.['part-list']?.['score-part']?.[0];
+    scorePart['part-name'] = instrName; //embed.instrumentName;
+    scorePart['part-abbreviation'] = instrName; //embed.instrumentAbbreviation;
+    scorePart['score-instrument']['instrument-name'] = instrName; //embed.instrumentName;
+    // console.log("bucket", bucket); //FIXME????
+
+    // // change the notes from tonic eb to whatever
+    // template?.["score-partwise"]?.part?.[0]?.measure?.[0]?.note?.forEach(
+    //   (note, i) => {
+    //     note.pitch.step = bucket[i].step;
+    //     note.pitch.octave = bucket[i].octave;
+    //     if (bucket[i].alter) {
+    //       note.pitch.alter = bucket[i].alter;
+    //     } else if (note.pitch.alter) {
+    //       delete note.pitch.alter;
+    //     }
+    //     console.log('note.pitch', note.pitch)
+    //   }
+    // );
+
+    // start from bucket, create the notes, add them to measure
+    template['score-partwise'].part[0].measure[0].note = bucket.map(
+      ({ alter, octave, step, $color = '#00000' }) => {
+        // console.log('\n\n\n\nmake note')
+        // console.log('alter, octave, step' );
+        // console.log(alter, octave, step );
+        // const  = noteInfo
+        const note = {
+          staff: '1',
+          voice: '1',
+          duration: '1',
+          pitch: { octave, step },
+          type: 'quarter',
+          $color,
+        };
+        if (alter !== '') {
+          note.pitch.alter = alter;
+        }
+        return note;
+      }
+    );
+    // console.log('\n\n\n\n\ntemplate["score-partwise"].part[0].measure[0].note');
+    // console.log(template["score-partwise"].part[0].measure[0].note);
+
+    // change the key signature in the score from whatever it is in tonic and eb to what we're given
+    template?.['score-partwise']?.part?.[0]?.measure?.[0]?.attributes?.forEach(
+      (element) => {
+        if (element.key) {
+          //FIXME
+          element.key.fifths = keySig.keyAsJSON.fifths;
+        }
+        if (element.clef) {
+          //FIXME
+          element.clef = keySig.clef;
+        }
+      }
+    );
+
+    if (octaveShift !== 0) {
+      template?.[
+        'score-partwise'
+      ]?.part?.[0]?.measure?.[0]?.attributes?.forEach((element) => {
+        element.transpose = {
+          chromatic: '0',
+          'octave-change': `${octaveShift}`,
+        };
+      });
+    }
+
+    const resultTransposed = embed.ready().then(() => {
+      return embed.loadJSON(template);
+    });
+    return resultTransposed;
+  };
+
+  useEffect(() => {
+    if (referenceScoreJSON !== '' && referenceScoreJSON !== undefined) {
+      // Makes sure we don't accidentally run this code and prevents runtime error.
+      const copyJSON = JSON.parse(referenceScoreJSON); // Creates our JSON object to use in our functions below.
+      function keyFromScoreJSON(pieceScoreJSON) {
+        const nonNegative = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#']; // moved from global scope.
+        const negatives = {
+          '-1': 'F',
+          '-2': 'Bb',
+          '-3': 'Eb',
+          '-4': 'Ab',
+          '-5': 'Db',
+          '-6': 'Gb',
+          '-7': 'Cb',
+        };
+        const CIRCLE_OF_FIFTHS = Object.assign({}, nonNegative, negatives);
+        const minOctave = pieceScoreJSON['score-partwise'][
+          'part'
+        ][0].measure.reduce(
+          // Gets the min octave within all the measures.
+          (measureMinOctave, measure) => {
+            const minForMeasure = measure.note.reduce((noteMinOctave, note) => {
+              // Gets the min octave within a measure.
+              if (note.pitch && note.pitch.octave !== undefined) {
+                // *FIX* Without this statement we get undefined values. Set a default to 0.
+
+                const thisOctave = parseInt(note.pitch.octave, 10);
+                if (thisOctave < noteMinOctave) {
+                  return thisOctave;
+                }
+                return noteMinOctave;
+              } else {
+                // If we get rid of this statement it moves all our octaves up to almost our result. The notes are now off by approx. 1 line.
+                return 100;
+              }
+            }, 100);
+
+            if (minForMeasure < measureMinOctave) {
+              return minForMeasure;
+            }
+            return measureMinOctave;
+          },
+          100
+        ); //TODO: what if octave can be higher than 10?
+        const keySignature = {
+          repr: CIRCLE_OF_FIFTHS[
+            pieceScoreJSON['score-partwise']['part'][0]['measure'][0][
+              'attributes'
+            ][0]['key']['fifths']
+          ],
+          keyAsJSON:
+            pieceScoreJSON['score-partwise']['part'][0]['measure'][0][
+              'attributes'
+            ][0]['key'],
+          clef: pieceScoreJSON['score-partwise']['part'][0]['measure'][0]
+            .attributes[0].clef,
+          minOctave,
+        };
+        return keySignature;
+      }
+      function getChordScaleInKey(chordScale, keyObj) {
+        const tonicBucketIntervals = [
+          // musical facts.
+          { name: '1', offset: 0 }, // Unison
+          { name: '2', offset: 2 }, // Major 2nd
+          { name: '3', offset: 4 }, // Major 3rd
+          { name: '5', offset: 7 }, // Perfect 5th
+          { name: '6', offset: 9 }, // Major 6th
+        ];
+
+        const subdominantBucketIntervals = [
+          { name: '1', offset: 0 }, // Unison
+          { name: '2', offset: 2 }, // Major 2nd
+          { name: '4', offset: 5 }, // Perfect 4th
+          { name: '5', offset: 7 }, // Perfect 5th
+          { name: '6', offset: 9 }, // Major 6th
+        ];
+
+        const dominantBucketIntervals = [
+          { name: '2', offset: 2 }, // Major 2nd
+          { name: '4', offset: 5 }, // Perfect 4th
+          { name: '5', offset: 7 }, // Perfect 5th
+          { name: '6', offset: 9 }, // Major 6th
+          { name: '7', offset: 11 }, // 7th
+        ];
+
+        const chordScaleIntervals = {
+          tonic: tonicBucketIntervals,
+          subdominant: subdominantBucketIntervals,
+          dominant: dominantBucketIntervals,
+        };
+
+        const noteToScaleIdx = {
+          C: 0,
+          'C#': 1,
+          Db: 1,
+          D: 2,
+          'D#': 3,
+          Eb: 3,
+          E: 4,
+          F: 5,
+          'F#': 6,
+          Gb: 6,
+          G: 7,
+          'G#': 8,
+          Ab: 8,
+          A: 9,
+          'A#': 10,
+          Bb: 10,
+          B: 11,
+          Cb: 11,
+        };
+
+        const chromaticScale = [
+          {
+            sharp: {
+              step: 'C',
+              alter: '',
+            },
+            flat: {
+              step: 'C',
+              alter: '',
+            },
+          },
+          {
+            sharp: {
+              repr: 'C#',
+              alter: '1',
+              step: 'C',
+            },
+            flat: {
+              repr: 'Db',
+              alter: '-1',
+              step: 'D',
+            },
+          },
+          {
+            sharp: {
+              repr: 'D',
+              alter: '',
+              step: 'D',
+            },
+            flat: {
+              repr: 'D',
+              alter: '',
+              step: 'D',
+            },
+          },
+          {
+            sharp: {
+              repr: 'D#',
+              alter: '1',
+              step: 'D',
+            },
+            flat: {
+              repr: 'Eb',
+              alter: '-1',
+              step: 'E',
+            },
+          },
+          {
+            sharp: {
+              repr: 'E',
+              alter: '',
+              step: 'E',
+            },
+            flat: {
+              repr: 'E',
+              alter: '',
+              step: 'E',
+            },
+          },
+          {
+            sharp: {
+              repr: 'F',
+              alter: '',
+              step: 'F',
+            },
+            flat: {
+              repr: 'F',
+              alter: '',
+              step: 'F',
+            },
+          },
+          {
+            sharp: {
+              repr: 'F#',
+              alter: '1',
+              step: 'F',
+            },
+            flat: {
+              repr: 'Gb',
+              alter: '-1',
+              step: 'G',
+            },
+          },
+          {
+            sharp: {
+              repr: 'G',
+              alter: '',
+              step: 'G',
+            },
+            flat: {
+              repr: 'G',
+              alter: '',
+              step: 'G',
+            },
+          },
+          {
+            sharp: {
+              repr: 'G#',
+              alter: '1',
+              step: 'G',
+            },
+            flat: {
+              repr: 'Ab',
+              alter: '-1',
+              step: 'A',
+            },
+          },
+          {
+            sharp: {
+              repr: 'A',
+              alter: '',
+              step: 'A',
+            },
+            flat: {
+              repr: 'A',
+              alter: '',
+              step: 'A',
+            },
+          },
+          {
+            sharp: {
+              repr: 'A#',
+              alter: '1',
+              step: 'A',
+            },
+            flat: {
+              repr: 'Bb',
+              alter: '-1',
+              step: 'B',
+            },
+          },
+          {
+            sharp: {
+              repr: 'B',
+              alter: '',
+              step: 'B',
+            },
+            flat: {
+              repr: 'Cb',
+              alter: '-1',
+              step: 'C',
+            },
+          },
+        ];
+
+        const key = keyObj.repr;
+        let alter = 'sharp';
+        if (key.includes('b') || key === 'F') {
+          // musician said so (that F is also flat, and that all else are sharp)
+          alter = 'flat';
+        }
+        const firstPitchIdx = noteToScaleIdx[key];
+        const firstPitchObj = chromaticScale[firstPitchIdx];
+        let octave = keyObj.minOctave;
+
+        // start from the pitch that represents the key of the score and find the 5 pitches with the corresponsing relationship from the chordScaleIntervals
+        const mapped = chordScaleIntervals[chordScale].map((interval) => {
+          if (
+            // if we reach the end of the array, go back to the front (circular array essentially) and increment octave
+            Math.floor(
+              (firstPitchIdx + interval.offset) / chromaticScale.length
+            ) > 0
+          ) {
+            octave = keyObj.minOctave + 1;
+          }
+
+          const result =
+            chromaticScale[
+              (firstPitchIdx + interval.offset) % chromaticScale.length
+            ][alter];
+          result.octave = '' + octave;
+          return result;
+        });
+        return mapped;
+      }
+      const keySignature = keyFromScoreJSON(copyJSON);
+      let bucket;
+      if (
+        chordScaleBucket === 'tonic' ||
+        chordScaleBucket === 'subdominant' ||
+        chordScaleBucket === 'dominant'
+      ) {
+        bucket = getChordScaleInKey(chordScaleBucket, keySignature);
+      } else {
+        bucket = getChordScaleInKey('tonic', keySignature);
+      }
+
+      if (colors) {
+        colorNotes(bucket, colors);
+      }
+
+      console.log('current JSON', copyJSON);
+      console.log('bucket', bucket); // current issue: our octaves are all defaulted to 0; however, to fix this issue if we comment out our else statement in our keyFromScoreJSON it fixes.
+      embedTransposed(bucket, embed, keySignature, instrumentName);
+    }
+  }, [referenceScoreJSON, chordScaleBucket]);
+
+  function colorNotes(notes, color) {
+    for (let i = 0; i < notes.length; i++) {
+      notes[i].$color = color;
+    }
+  }
   /**
    *  Given a measure, and a string consisting of "rgbgrb..." we match the notes of the corresponding measure to that value.
    *  For example, given a measure (1,2,3) and a string "rgb", the first measure would be colored red, second would be green, third would be green.
@@ -51,16 +515,15 @@ function FlatEditor({
     /**
      * Colors an array of notes to a given hex color attribute.
      */
-    const colorNotes = (notes, color) => {
-      for (let i = 0; i < notes.length; i++) {
-        notes[i].$color = color;
-      }
-    };
     const BLACK = '#000000';
     const ORANGE = '#f5bd1f';
     for (let i = 0; i < measures.length; i++) {
-      if (colorSpecs && colorSpecs[i]) {
-        colorNotes(measures[i].note, colorSpecs[i]);
+      if (colorSpecs) {
+        if (Array.isArray(colorSpecs) && colorSpecs[i]) {
+          colorNotes(measures[i].note, colorSpecs[i]);
+        } else if (!Array.isArray(colorSpecs)) {
+          colorNotes(measures[i].note, colorSpecs);
+        }
       } else {
         colorNotes(measures[i], BLACK);
       }
@@ -156,7 +619,7 @@ function FlatEditor({
               );
             }
             return (
-              // edit &&
+              // if a user adds a note that is black or does not have a color assigned to it, then we apply the color from the chord scale pattern to match.
               score.scoreId === 'blank' &&
               embed.loadJSON(result).then(() => {
                 embed.off('noteDetails');
@@ -164,7 +627,7 @@ function FlatEditor({
                   // console.log('noteDetails', info);
                   embed.getJSON().then((jd) => {
                     const jsonData = jd;
-                    console.log('on noteDetails', JSON.stringify(jsonData));
+                    // console.log('on noteDetails', JSON.stringify(jsonData));
                     // console.log('jsonData', jsonData);
                     // console.log(jsonData['score-partwise'].part[0].measure.flatMap((m) => m.note.map((n) => n.$color)))
                     if (
@@ -173,15 +636,19 @@ function FlatEditor({
                         m.note.some((n) => !n.$color || n.$color === '#000000')
                       )
                     ) {
-                      console.log('we need to recolor');
+                      // console.log('we need to recolor');
                       jsonData['score-partwise'].part[0].measure =
                         colorMeasures(
                           jsonData['score-partwise'].part[0].measure,
                           colors
                         );
-                      embed.getCursorPosition().then((position) => embed
-                        .loadJSON(jsonData)
-                        .then(() => embed.setCursorPosition(position)));
+                      embed
+                        .getCursorPosition()
+                        .then((position) =>
+                          embed
+                            .loadJSON(jsonData)
+                            .then(() => embed.setCursorPosition(position))
+                        );
                     }
                     const data = JSON.stringify(jsonData);
                     // validateScore(jsonData, [])
@@ -214,32 +681,7 @@ function FlatEditor({
                 console.error('score not loaded from scoreId');
                 console.error(e);
               })
-        )
-        .then(() => {
-          if (edit) {
-            // embed.off('measureDetails');
-            // embed.on('measureDetails', (info) => {
-            //   console.log('measureDetails', info);
-            //   // if (colors && addingNote) {
-            //   //   embed.getJSON().then((jd) => {
-            //   //     const jsonData = jd;
-            //   //     jsonData['score-partwise'].part[0].measure = colorMeasures(
-            //   //       jsonData['score-partwise'].part[0].measure,
-            //   //       colors
-            //   //     );
-            //   //     const data = JSON.stringify(jsonData);
-            //   //     if (json !== data) {
-            //   //       if (addingNote) {
-            //   //         setAddingNote(false);
-            //   //         embed.loadJSON(jsonData);
-            //   //       }
-            //   //     }
-            //   //   });
-            //   //   setAddingNote(true);
-            //   // }
-            // });
-          }
-        });
+        );
     } else if (scoreJSON && embed) {
       // this is currently for the grade creativity screen
       // console.log('loadJSON', scoreJSON)
@@ -266,7 +708,7 @@ function FlatEditor({
           console.error(e);
         });
     }
-  }, [score, scoreJSON, embed]);
+  }, [score, scoreJSON, chordScaleBucket, embed]);
 
   return (
     <>
--- components/student/create/aural.js
+++ components/student/create/aural.js
@@ -20,7 +20,7 @@ import { UploadStatusEnum } from '../../../types';
 const FlatEditor = dynamic(() => import('../../flatEditor'), {
   ssr: false,
 });
-// test.
+
 const bucketColors = {
   tonic: '#E75B5C',
   subdominant: '#265C5C',
@@ -29,7 +29,7 @@ const bucketColors = {
 };
 
 export default function CreativityAuralActivity() {
-  console.log('got into aural component');
+  // console.log('got into aural component');
   const dispatch = useDispatch();
   // I think this should show the melody for the current piece, but in the student's transposition
   // need to get the student's current assignment
@@ -106,57 +106,50 @@ export default function CreativityAuralActivity() {
         submissionId,
       })
     );
-  console.log('flatIOScoreForTransposition', flatIOScoreForTransposition);
+  // console.log('flatIOScoreForTransposition', flatIOScoreForTransposition);
   let scoreJSON;
   if (flatIOScoreForTransposition) {
     scoreJSON = JSON.parse(flatIOScoreForTransposition);
   }
-
+  // console.log(scoreJSON)
   // const origJSON
+
   return flatIOScoreForTransposition ? (
     <>
       <FlatEditor score={scoreJSON} giveJSON={setJson} />
-      {/* TODO: if the student has already submitted this, do we show their submission here? if so how would they start over? */}
+
+      {/**Testing row */}
       <Row>
         <Col md={4}>
-          {/* 
-          these 3 flat editors should not pull a specific scoreId
-          rather they should use the code we've been working on in the variations repo to dynamically produce the pitches
-          see eg https://github.com/hcientist/variations-on-motive/blob/88d4c55154fe2d136f43b4325fc60b0938fc1806/verify-chordScale-for-transposition.html#L214
-           */}
           <FlatEditor
             height={150}
-            score={{
-              scoreId: '65241135b67581e78952d1b1',
-              sharingKey:
-                '223faaebf63c6ff5c964fb74737554f58d86b99262bc62bac15195b66d3ee566f8ff923f6d094e611a610105b3d9582cea55e183cf60ab8683986fc29d7a1f37',
-            }}
-            colors={[bucketColors.tonic]}
+            referenceScoreJSON={json}
+            chordScaleBucket="tonic"
+            colors={bucketColors.tonic}
+            instrumentName={currentAssignment?.instrument}
           />
         </Col>
         <Col md={4}>
           <FlatEditor
             height={150}
-            score={{
-              scoreId: '6524114afc390c181375fdc8',
-              sharingKey:
-                'ea5a2d5bdb5b8c570cb0796add8188d50757c7a06d2636f1b7c088b905aa7716b8a8eaf0a0b12802d8a02852691b0420bff1adef17e7250bbe6f03b442131fda',
-            }}
-            colors={[bucketColors.subdominant]}
+            referenceScoreJSON={json}
+            chordScaleBucket="subdominant"
+            colors={bucketColors.subdominant}
+            instrumentName={currentAssignment?.instrument}
           />
         </Col>
         <Col md={4}>
           <FlatEditor
             height={150}
-            score={{
-              scoreId: '6524114e605572ddb1c1090f',
-              sharingKey:
-                '895d5f93344d05f47252535852e3f9f9ec638b94b3237278508cf6b085671dc56791d83fba37fc1c0285fec49a70a417a461d49f4ec48c520a96a6b076bab21f',
-            }}
-            colors={[bucketColors.dominant]}
+            referenceScoreJSON={json}
+            chordScaleBucket="dominant"
+            colors={bucketColors.dominant}
+            instrumentName={currentAssignment?.instrument}
           />
         </Col>
       </Row>
+
+      {/* TODO: if the student has already submitted this, do we show their submission here? if so how would they start over? */}
       <FlatEditor
         edit
         score={{
@@ -165,11 +158,11 @@ export default function CreativityAuralActivity() {
         onSubmit={setJsonWrapper}
         submittingStatus={mutation.status}
         onUpdate={(data) => {
-          console.log('updated composition', data);
+          // console.log('updated composition', data);
           composition = data;
         }}
         orig={json}
-        colors={currentAssignment?.part?.chord_scale_pattern.map(
+        colors={currentAssignment?.part?.chord_scale_pattern?.map(
           (color) => bucketColors[color]
         )}
       />
