88689ccdbea0ae6214842ecaac57afa303d795a5
Michael Stewart <stewarmc@jmu.edu>
2024-03-11 12:26:44 -0400

    create PiecePicker Component

---
+++ components/student/piecePicker.js
@@ -0,0 +1,59 @@
+import { useRouter } from "next/router";
+import { Form, Nav, Spinner } from "react-bootstrap";
+import { useQuery } from "react-query";
+import { getStudentAssignments } from "../../api";
+
+function PiecePicker() {
+  function onPieceChange(ev) {
+    console.log('onpiecechange', ev.target.value)
+    router.push(`/courses/${slug}/${ev.target.value}`)
+  }
+  const router = useRouter();
+  const { slug, piece } = router.query;
+  const {
+    isLoading,
+    error: assignmentsError,
+    data: assignments,
+  } = useQuery('assignments', getStudentAssignments(slug), {
+    enabled: !!slug,
+  });
+
+  const pieces = assignments && assignments ? Object.keys(assignments).map(pieceSlug => {
+    return {
+      pieceSlug,
+      pieceName: assignments[pieceSlug][0].piece_name,
+    }
+  }) : null;
+
+  if (!pieces && isLoading) {
+    return <Nav.Item><Spinner
+      as="span"
+      animation="border"
+      size="sm"
+      role="status"
+      aria-hidden="true"
+      variant="light"
+    >
+      <span className="visually-hidden">Loading...</span>
+    </Spinner></Nav.Item>
+  }
+
+  if (!pieces) {
+    return null;
+  }
+
+  if (pieces.length === 1) {
+    return null
+  }
+
+  return <Nav.Item className="me-1">
+    <Form.Select onChange={onPieceChange} defaultValue={piece}>
+      <option value="">Choose music</option>
+      {pieces.map(piece => <option key={piece.pieceSlug} value={piece.pieceSlug}>{piece.pieceName}</option>)}
+    </Form.Select>
+  </Nav.Item>
+}
+
+export {
+  PiecePicker,
+};
