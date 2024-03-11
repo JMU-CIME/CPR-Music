9a1fc5520fe248be7c7da20428047088385b459c
Michael Stewart <stewarmc@jmu.edu>
2024-03-11 12:24:52 -0400

    create CourseSelector component

--- components/courseSelector.js
+++ components/courseSelector.js
@@ -3,14 +3,13 @@ import { getEnrollments } from "../api";
 import Form from "react-bootstrap/Form";
 import { useRouter } from "next/router";
 import Spinner from "react-bootstrap/Spinner";
+import { Nav } from "react-bootstrap";
+import Link from "next/link";
 
 
 function CourseSelector() {
-  let navToCourse;
   const router = useRouter();
   const changeCourse = (ev) => {
-    console.log('changeCourse', ev)
-    console.log('navToCourse', navToCourse)
     router.push(`/courses/${ev.target.value}`)
   }
   const { slug } = router.query;
@@ -20,31 +19,49 @@ function CourseSelector() {
     enrollments ? enrollments.filter((elem) => elem.course.slug === slug)[0] : null;
 
   const selectedCourse = currentEnrollment?.course;
-  console.log('slug', slug);
-  console.log('currentEnrollment', currentEnrollment);
-  console.log('selectedCourse', selectedCourse);
-  console.log('selectedCourse?.slug', selectedCourse?.slug);
-  console.log('!!enrollments && isLoading', !!enrollments && isLoading)
-  console.log('enrollments && selectedCourse', enrollments && selectedCourse)
-  console.log('enrollments', enrollments)
-  
-  return !enrollments && isLoading ?
-    <Spinner
+
+  // if the enrollments haven't loaded yet, show a spinner
+  if (!enrollments && isLoading) {
+    return <Nav.Item><Spinner
       as="span"
       animation="border"
       size="sm"
       role="status"
       aria-hidden="true"
+      variant="light"
+    >
+      <span className="visually-hidden">Loading...</span>
+    </Spinner></Nav.Item>
+  }
+  
+  // if the user has only one enrollment, don't show the dropdown
+  if (enrollments && enrollments.length === 1) {
+    return <Nav.Item><Link href={`/courses/${enrollments[0].course.slug}`} passHref>
+      <Nav.Link>{ enrollments[0].course.name }</Nav.Link>
+    </Link></Nav.Item>
+  }
+
+  //probably don't need the spinner here?
+  return <Nav.Item>
+    {!enrollments && isLoading ?
+    <Spinner
+      as="span"
+      animation="border"
+      size="sm"
+      role="status"
+        aria-hidden="true"
+        variant="light"
     >
       <span className="visually-hidden">Loading...</span>
     </Spinner> : (enrollments && (
-      <Form.Select onChange={changeCourse} defaultValue={selectedCourse?.slug} value={navToCourse}>
-        { enrollments && <option>Choose your course</option>}
+        <Form inline><Form.Select onChange={changeCourse} defaultValue={selectedCourse?.slug} className="me-1">
+        { enrollments && <option value="" disabled={!!slug}>Choose your course</option>}
         { enrollments && enrollments.map((enrollment) => (
           <option key={enrollment.course.slug} value={enrollment.course.slug}>{enrollment.course.name}</option> 
         ))}
-      </Form.Select>
-    ))
+      </Form.Select></Form>
+    ))}
+    </Nav.Item>
 }
 
 export default CourseSelector;
