0bf7461f7bed5afcc80431fc925e580f7cc87e7f
Adam Vinch <adamvinch@gmail.com>
2024-04-17 21:28:35 -0400

    Add request to backend to change instrument. Only display instrument selector if the course allows students to change instruments

--- components/instrumentSelector.js
+++ components/instrumentSelector.js
@@ -1,8 +1,10 @@
 import { fetchInstruments } from "../actions";
 import Form from "react-bootstrap/Form";
 import { useDispatch, useSelector } from 'react-redux';
+import Spinner from "react-bootstrap/Spinner";
 
-export default function InstrumentSelector({defaultInstrument}) {
+
+export default function InstrumentSelector({defaultInstrument, onChange}) {
   let { items: instruments, loaded: instrumentsLoaded } = useSelector(
     (state) => state.instruments
   );
@@ -11,15 +13,26 @@ export default function InstrumentSelector({defaultInstrument}) {
     const dispatch = useDispatch();
     instruments = dispatch(fetchInstruments());
   }
-  
-  return (
-    <Form.Select size="sm">
-      {defaultInstrument && <option>{defaultInstrument}</option>}
+
+  return instrumentsLoaded ? (
+    <Form.Select 
+      size="sm"
+      onChange={(e) => onChange(instruments[e.target.value])}
+      defaultValue={Object.values(instruments).find(i => i.name === defaultInstrument).id}
+    >
       {instruments && (
-        Object.values(instruments).map((instrument, index) => (
-          <option key={index}>{instrument.name}</option>
+        Object.values(instruments).map((instrument) => (
+          <option
+            key={instrument.id} 
+            value={instrument.id}
+          >{instrument.name}</option>
         ))
       )} 
     </Form.Select>
+  ) : (
+    <Spinner 
+      size="sm"
+      variant="primary"
+    />
   )
 }
