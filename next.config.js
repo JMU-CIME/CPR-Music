4c8277bf0bf9ff4bd38cf93af761584e7b21bc8f
Michael Stewart <stewarmc@jmu.edu>
2024-05-30 23:22:17 -0400

    not sure what this rewrite was for, but it seems like we don't need it

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/auth-token/:rest*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_HOST}/:rest*`,
-      },
-      {
         source: '/backend/:rest*',
         destination: `${process.env.NEXT_PUBLIC_BACKEND_HOST}/:rest*`,
       },
